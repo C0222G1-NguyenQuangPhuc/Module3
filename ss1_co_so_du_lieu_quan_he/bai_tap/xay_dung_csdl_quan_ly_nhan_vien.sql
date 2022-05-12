c674fdccdbd60087a2b8962aa8e25b5bf187c657
C0222G1-NguyenQuangPhuc <phucprokoten@gmail.com>
2022-05-12 11:45:25 +0700

    up bai tap ngay 12/05/2022

--- ss1_co_so_du_lieu_quan_he/bai_tap/xay_dung_csdl_quan_ly_nhan_vien.sql
+++ ss1_co_so_du_lieu_quan_he/bai_tap/xay_dung_csdl_quan_ly_nhan_vien.sql
@@ -1,21 +1,21 @@
-create database `student-management`;
-use `student-management`;
+create database `student_management`;
+use `student_management`;
 CREATE TABLE `student` (
-    id INT,
-    name VARCHAR(45),
-    age INT,
-    country VARCHAR(45),
+    id INT NOT NULL ,
+    name VARCHAR(45) NULL,
+    age INT NULL,
+    country VARCHAR(45) NULL,
     PRIMARY KEY (id)
 );
 create table `class`(
-	id int,
-    name varchar(45),
+	id int not null,
+    name varchar(45) null,
     primary key (id)
 );
 create table `teacher`(
-	id int,
-    name varchar(45),
-    age int,
-    country varchar(45),
+	id int not null,
+    name varchar(45) null,
+    age int null,
+    country varchar(45) null,
     primary key (id)
 );
